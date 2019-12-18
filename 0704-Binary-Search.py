2087b937f2a3af0b2aaeb56f4c1d95cdb736ba9b
nirmalnishant645 <nirmalnishant645@gmail.com>
2019-12-19 00:34:02 +0530

    Binary Search

--- 0704-Binary-Search.py
+++ 0704-Binary-Search.py
@@ -1,4 +1,5 @@
 '''
+https://leetcode.com/problems/binary-search/
 Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
 
 
