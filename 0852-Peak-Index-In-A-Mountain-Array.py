c4336f34fb601f266d55d075d51e8f83a36964e1
nirmalnishant645 <nirmalnishant645@gmail.com>
2019-12-25 00:10:46 +0530

    Peak Index in a Mountain Array

---
+++ 0852-Peak-Index-In-A-Mountain-Array.py
@@ -0,0 +1,34 @@
+'''
+https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+Let's call an array A a mountain if the following properties hold:
+A.length >= 3
+There exists some 0 < i < A.length - 1 such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+Given an array that is definitely a mountain, return any i such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].
+
+Example 1:
+Input: [0,1,0]
+Output: 1
+
+Example 2:
+Input: [0,2,1,0]
+Output: 1
+
+Note:
+3 <= A.length <= 10000
+0 <= A[i] <= 10^6
+A is a mountain, as defined above.
+'''
+
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        lower, upper = 0, len(A)-1
+
+        while(lower<upper):
+            mid = (lower+upper)//2
+            if (A[mid]>A[mid+1]):
+                upper = mid
+            else:
+                lower = mid+1
+        return upper
+    
