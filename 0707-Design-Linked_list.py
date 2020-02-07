06f890d04507e8f31fcb5552da27ff1b3c76fe72
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-07 14:35:05 +0530

    Design Linked List

--- 0707-Design-Linked_list.py
+++ 0707-Design-Linked_list.py
@@ -34,7 +34,7 @@ Constraints:
 Please do not use the built-in LinkedList library.
 At most 2000 calls will be made to get, addAtHead, addAtTail,  addAtIndex and deleteAtIndex.
 '''
-class Node:
+cclass Node:
     def __init__(self, data=None):
         self.data = data
         self.next = None
@@ -49,7 +49,7 @@ class MyLinkedList:
         self.tail = None
         self.size = 0
 
-    def get(self, index: int) -> int: # Time Complexity: O(n)
+    def get(self, index: int) -> int:
         """
         Get the value of the index-th node in the linked list. If the index is invalid, return -1.
         """
@@ -61,7 +61,7 @@ class MyLinkedList:
         return cur_node.data
         
 
-    def addAtHead(self, val: int) -> None: # Time Complexity: O(1)
+    def addAtHead(self, val: int) -> None:
         """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
         """
@@ -75,7 +75,7 @@ class MyLinkedList:
         self.size += 1
         
 
-    def addAtTail(self, val: int) -> None: # Time Complexity: O(1)
+    def addAtTail(self, val: int) -> None:
         """
         Append a node of value val to the last element of the linked list.
         """
@@ -89,7 +89,7 @@ class MyLinkedList:
         self.size += 1
         
 
-    def addAtIndex(self, index: int, val: int) -> None: # Time Complexity: O(n)
+    def addAtIndex(self, index: int, val: int) -> None:
         """
         Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
         """
@@ -109,27 +109,21 @@ class MyLinkedList:
             self.size += 1
         
 
-    def deleteAtIndex(self, index: int) -> None: # Time Complexity: O(n)
+    def deleteAtIndex(self, index: int) -> None:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
         if index >= self.size:
             return
-        if index == 0:
-            if self.head is self.tail:
-                self.head = self.tail = None
-            else:
-                self.head = self.head.next
+        elif index == 0:
+            self.head = self.head.next
             self.size -= 1
         elif index == self.size - 1:
-            if self.tail is self.head:
-                self.tail = self.head = None
-            else:
-                cur_node = self.head
-                while cur_node.next != self.tail:
-                    cur_node = cur_node.next
-                cur_node.next = None
-                self.tail = cur_node
+            cur_node = self.head
+            while cur_node.next != self.tail:
+                cur_node = cur_node.next
+            cur_node.next = None
+            self.tail = cur_node
             self.size -= 1
         else:
             cur_node = self.head
