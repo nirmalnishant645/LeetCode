6edc2649fb2ffe3d59bac12a33a7a57e3887da3d
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-10 11:47:30 +0530

    Reverse Linked List

--- 0206-Reverse-Linked-List.py
+++ 0206-Reverse-Linked-List.py
@@ -16,11 +16,12 @@ A linked list can be reversed either iteratively or recursively. Could you imple
 #         self.val = x
 #         self.next = None
 
+#Iteratively
 # Time Complexity - O(n), Space Complexity - O(1)
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        temp = None
+        prev = None
         cur_node = head
         while cur_node:
-            cur_node.next, temp, cur_node = temp, cur_node, cur_node.next
-        return temp
+            cur_node.next, prev, cur_node = prev, cur_node, cur_node.next
+        return prev
