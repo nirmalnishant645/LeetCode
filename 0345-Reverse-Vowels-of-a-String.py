88026d5aa7fcd87012da585f60d8cb99dfe22f10
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-15 14:27:52 +0530

    Reverse vowels of string

--- 0345-Reverse-Vowels-of-a-String.py
+++ 0345-Reverse-Vowels-of-a-String.py
@@ -16,8 +16,7 @@ The vowels does not include the letter "y".
 '''
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels = set()
-        vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
         string = list(s)
         start, end = 0, len(s) - 1
         while start <= end:
@@ -25,9 +24,7 @@ class Solution:
                 if string[end] in vowels:
                     string[start], string[end] = string[end], string[start]
                     start += 1
-                    end -= 1
-                else:
-                    end -= 1
+                end -= 1
             else:
                 start += 1
         return ''.join(string)
