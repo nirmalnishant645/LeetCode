6d97206db314d898ead026a95bff233a4cb86a23
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-19 23:03:08 +0530

    Roman to Integer

--- 0013-Roman-to-Integer.py
+++ 0013-Roman-to-Integer.py
@@ -50,7 +50,7 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        res, prev = 0, 0
+        res = prev = 0
         for i in range(len(s) - 1, -1, -1):
             if roman[s[i]] >= prev:
                 res += roman[s[i]]
