942348f2b9a8e9e60842142c65145473084509d2
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-19 23:15:43 +0530

    Palindrome check using two pointers

--- 0125-Valid-Palindrome.py
+++ 0125-Valid-Palindrome.py
@@ -16,15 +16,21 @@ Output: false
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         first, last = 0, len(s) - 1
+        
         while first < last:
+            
             if not s[first].isalnum():
                 first += 1
                 continue
+            
             elif not s[last].isalnum():
                 last -= 1
                 continue
+            
             elif s[first].lower() != s[last].lower():
                 return False
+            
             first += 1
             last -= 1
+        
         return True
