5d10ab7a1279e6e147adcec0ec38d09f60a07194
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-19 23:14:25 +0530

    Roman to Integer

--- 0013-Roman-to-Integer.py
+++ 0013-Roman-to-Integer.py
@@ -52,9 +52,7 @@ class Solution:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         res = prev = 0
         for i in range(len(s) - 1, -1, -1):
-            if roman[s[i]] >= prev:
-                res += roman[s[i]]
-            else:
-                res -= roman[s[i]]
+            cur = roman[s[i]]
+            res = res + cur if cur >= prev else res - cur
             prev = roman[s[i]]
         return res
