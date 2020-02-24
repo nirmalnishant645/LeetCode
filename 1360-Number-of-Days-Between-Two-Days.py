1b4e8ff29e9cd04e804913d71b3780ceed4ab24c
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-24 14:27:18 +0530

    Number of Days between dates

--- 1360-Number-of-Days-Between-Two-Days.py
+++ 1360-Number-of-Days-Between-Two-Days.py
@@ -19,3 +19,39 @@ Constraints:
 
 The given dates are valid dates between the years 1971 and 2100.
 '''
+class Solution:
+    def daysBetweenDates(self, date1: str, date2: str) -> int:
+        year1 = year2 = month1 = month2 = day1 = day2 = ''
+        for i in range(len(date1)):
+            if i < 4:
+                year1 += date1[i] 
+                year2 += date2[i] 
+            elif 4 < i < 7:
+                month1 += date1[i]
+                month2 += date2[i]
+            elif 7 < i:
+                day1 += date1[i]
+                day2 += date2[i]
+        year1, year2, month1, month2, day1, day2 = int(year1), int(year2), int(month1), int(month2), int(day1), int(day2)
+        
+        days1, days2 = self.countDays(day1, month1, year1), self.countDays(day2, month2, year2)
+        
+        return days2 - days1 if days2 >= days1 else days1 - days2
+        
+    def countDays(self, day, month, year):
+        months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        count = 0
+        
+        for i in range(1970, year):
+            if (i % 100 and not i % 4) or not i % 400:
+                count += 366
+            else:
+                count += 365
+        
+        for i in range(month - 1):
+            count += months[i]
+            
+        if ((year % 100 and not year % 4) or not year % 400) and (month > 2 or (month == 2 and day == 29)):
+            count += 1
+            
+        return count + day
