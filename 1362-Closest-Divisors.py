8ae49cccc105b2d26e09093b0c1e05b17c7fe357
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-27 09:21:15 +0530

    Closest Divisors

--- 1362-Closest-Divisors.py
+++ 1362-Closest-Divisors.py
@@ -29,7 +29,7 @@ Constraints:
 '''
 class Solution:
     def closestDivisors(self, num: int) -> List[int]:
-        for i in range(int(math.sqrt(num + 2)), 0, -1):
+        for i in range(int((num + 2)**0.5), 0, -1):
             if not (num + 1) % i:
                 return [i, (num + 1) // i]
             if not (num + 2) % i:
