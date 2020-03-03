8e164b3d62ca200c055ac6718469fd96a036cfcb
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-03-03 15:27:40 +0530

    Update 0121-Best-Time-To-Buy-and-Sell-Stock.py

--- 0121-Best-Time-To-Buy-and-Sell-Stock.py
+++ 0121-Best-Time-To-Buy-and-Sell-Stock.py
@@ -21,7 +21,7 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices: return 0
         min_price, max_profit = prices[0], 0
-        for idx, val in enumerate(prices):
-            min_price = val if val < min_price else min_price
-            max_profit = val - min_price if val - min_price > max_profit else max_profit
+        for price in prices:
+            min_price = price if price < min_price else min_price
+            max_profit = price - min_price if price - min_price > max_profit else max_profit
         return max_profit
