21e7306ea341845090af494866e7e541cdc1cb9a
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-03-03 16:12:08 +0530

    Update 0283-Move-Zeroes.py

--- 0283-Move-Zeroes.py
+++ 0283-Move-Zeroes.py
@@ -12,9 +12,12 @@ You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 '''
 class Solution:
-        def moveZeroes(self, nums: List[int]) -> None:
-            flag = 0
-            for i in range(len(nums)):
-                if nums[i]!=0:
-                    nums[i], nums[flag] = nums[flag], nums[i]
-                    flag+=1
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
