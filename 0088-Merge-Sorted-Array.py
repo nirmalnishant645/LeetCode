540b29e4118df6e68a6860ed64c7d31449813ed7
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-03-03 19:48:08 +0530

    Update 0088-Merge-Sorted-Array.py

--- 0088-Merge-Sorted-Array.py
+++ 0088-Merge-Sorted-Array.py
@@ -23,17 +23,15 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         last = m + n - 1
-        m -= 1
-        n -= 1
+        m, n = m - 1, n - 1
         for i in range(last, -1, -1):
-            if m >= 0 and n >= 0:
-                arg1 , arg2 = nums1[m], nums2[n]
-                if arg2 > arg1:
+            if n >= 0 and m >= 0:
+                if nums2[n] > nums1[m]:
                     nums1[i] = nums2[n]
                     n -= 1
                 else:
                     nums1[i] = nums1[m]
-                    m -=1
+                    m -= 1
             elif n >= 0:
                 nums1[i] = nums2[n]
-                n = n - 1
+                n -= 1
