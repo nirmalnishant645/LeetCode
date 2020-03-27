549f8737e1a8dc6c394db5ad7f26a9a5317ac55e
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-03-27 12:32:58 +0530

    added complexity

--- 0739-Daily-Temperatures.py
+++ 0739-Daily-Temperatures.py
@@ -5,6 +5,8 @@ For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73]
 
 Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100]. 
 '''
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
         res = [0] * len(T)
