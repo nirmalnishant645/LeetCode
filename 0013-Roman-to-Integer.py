5abc0e9771ba2c9af0be26f5bf9c900dcbcb9921
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-03-31 14:02:32 +0530

    Update 0013-Roman-to-Integer.py

--- 0013-Roman-to-Integer.py
+++ 0013-Roman-to-Integer.py
@@ -47,6 +47,8 @@ Input: "MCMXCIV"
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 '''
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
