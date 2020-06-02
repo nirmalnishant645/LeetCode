2b474b1e2381ae6faa5866259a28f3dca2020cd1
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-06-02 13:16:06 +0530

    Binary Tree Level Order Traversal II

--- 0107-Binary-Tree-Level-Order-Traversal-II.py
+++ 0107-Binary-Tree-Level-Order-Traversal-II.py
@@ -42,3 +42,31 @@ class Solution:
             current = nex
             res.append(temp)
         return res[::-1]
+
+# Method 2, Recursive Solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        levels = []
+        if not root:
+            return levels
+
+        def helper(node,level):
+
+            if len(levels)==level:
+                levels.append([])
+
+            levels[level].append(node.val)
+
+            if node.left: helper(node.left, level+1)
+            if node.right: helper(node.right, level+1)
+
+        helper(root,0)
+
+        return levels[::-1]
