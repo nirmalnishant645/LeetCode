e5d212ea51c798baa9f5115c4545a8656f678513
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-08-06 13:12:10 +0530

    Maximum Subarray (Kadane's Algorithm)

--- 0053-Maximum-Subarray.py
+++ 0053-Maximum-Subarray.py
@@ -10,24 +10,17 @@ Follow up:
 
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 '''
-#Method 1
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums)==0:
-            return 0
-        summ = maxSum = nums[0]
-        for i in range(1, len(nums)):
-            summ = nums[i] if summ<0 else summ+nums[i]
-            maxSum = summ if summ>maxSum else maxSum
-        return maxSum
 
-#Method 2
+# Using Kadane's Algorithm
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        
         cur_sum = max_sum = nums[0]
+        
         for i in range(1, len(nums)):
-            cur_sum = nums[i] if nums[i] > cur_sum + nums[i] else cur_sum + nums[i]
-            max_sum = cur_sum if cur_sum > max_sum else max_sum
+            
+            cur_sum = max(nums[i] + cur_sum, nums[i])
+            max_sum = max(cur_sum, max_sum)
+            
         return max_sum
