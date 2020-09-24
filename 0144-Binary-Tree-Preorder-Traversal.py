4d1593ee38294ac32146c20bf5b8969e454971ee
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-09-24 18:39:31 +0530

    Update 0144-Binary-Tree-Preorder-Traversal.py
    Iterative Solution

--- 0144-Binary-Tree-Preorder-Traversal.py
+++ 0144-Binary-Tree-Preorder-Traversal.py
@@ -33,3 +33,19 @@ class Solution:
         res.append(root.val)
         self.recursive(root.left, res)
         self.recursive(root.right, res)
+      
+# Iterative Solution
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if not root:
+            return res
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
