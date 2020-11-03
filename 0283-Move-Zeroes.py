4dada4fb7696ff595caae27f6c2b45aa2f77f175
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-03 23:34:21 +0530

    Move Zeroes

--- 0283-Move-Zeroes.py
+++ 0283-Move-Zeroes.py
@@ -21,3 +21,13 @@ class Solution:
             if nums[i]:
                 nums[i], nums[zero] = nums[zero], nums[i]
                 zero += 1
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        non_zero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero] = nums[i]
+                non_zero += 1
+        for i in range(non_zero, len(nums)):
+            nums[i] = 0
