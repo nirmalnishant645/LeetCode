e258e3d89831205bd6723414bc1ac2d6972c24a0
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-05 18:24:43 +0530

    Valid Mountain Array Solution

--- 0941-Valid-Mountain-Array.py
+++ 0941-Valid-Mountain-Array.py
@@ -25,3 +25,18 @@ Constraints:
 0 <= arr[i] <= 104
 '''
 
+from typing import List
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+        i = 1
+        while i < len(arr) and arr[i] > arr[i-1]:
+            i += 1
+        if i == 1 or i == len(arr):
+            return False
+        while i < len(arr) and arr[i] < arr[i-1]:
+            i += 1
+        return i == len(arr)
+
