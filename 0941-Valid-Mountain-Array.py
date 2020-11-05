3cd80f802cfc148cdce9c62e35acd7bd29e911e8
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-05 18:24:26 +0530

    Valid Mountain Array

---
+++ 0941-Valid-Mountain-Array.py
@@ -0,0 +1,27 @@
+'''
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+
+Example 1:
+Input: arr = [2,1]
+Output: false
+
+Example 2:
+Input: arr = [3,5,5]
+Output: false
+
+Example 3:
+Input: arr = [0,3,2,1]
+Output: true
+ 
+Constraints:
+1 <= arr.length <= 104
+0 <= arr[i] <= 104
+'''
+
