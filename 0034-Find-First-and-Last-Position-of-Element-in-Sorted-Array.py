deff1054bc167ec6f956e8a437e38cc40fa4e88e
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-07 19:53:57 +0530

    First and Last Position Two Binary Search Funcs

--- 0034-Find-First-and-Last-Position-of-Element-in-Sorted-Array.py
+++ 0034-Find-First-and-Last-Position-of-Element-in-Sorted-Array.py
@@ -61,3 +61,41 @@ class Solution:
                 left = mid + 1
                 
         return [first, last]
+
+# Simple Binary Search in Two Different Functions
+
+class Solution:
+    
+    def getFirstOccurence(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                if mid == 0 or nums[mid - 1] != target:
+                    return mid
+                right = mid - 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+                
+    def getLastOccurence(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                if mid == len(nums) - 1 or nums[mid + 1] != target:
+                    return mid
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+    
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first = self.getFirstOccurence(nums, target)
+        last = self.getLastOccurence(nums, target)
+        
+        return [first, last]
