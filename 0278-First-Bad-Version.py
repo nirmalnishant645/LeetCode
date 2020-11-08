f65082aa848c2bad60a8d7c1cd29ad278f00b64d
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-08 23:13:11 +0530

    First Bad Version

--- 0278-First-Bad-Version.py
+++ 0278-First-Bad-Version.py
@@ -15,23 +15,25 @@ call isBadVersion(4) -> true
 
 Then 4 is the first bad version.
 '''
+
 # The isBadVersion API is already defined for you.
 # @param version, an integer
-# @return a bool
+# @return an integer
 # def isBadVersion(version):
 
 class Solution:
+
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
-        start, end = 1, n
-        while start < end:
-            mid = (start + end)//2
-
+        low, high = 1, n
+        while low < high:
+            mid = (low + high) // 2
             if isBadVersion(mid):
-                end = mid
+                high = mid
             else:
-                start = mid + 1
-        return end
+                low = mid + 1
+                
+        return high
