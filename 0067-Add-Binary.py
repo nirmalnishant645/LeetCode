abf941bbc13d5c5b8557304dd1ac186db2beca50
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-21 14:30:58 +0530

    Add Binary Method 2

--- 0067-Add-Binary.py
+++ 0067-Add-Binary.py
@@ -34,3 +34,21 @@ class Solution:
             j -= 1 
 
         return ''.join(res[::-1])
+
+# Method 2
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i, j, carry, result = len(a) - 1, len(b) - 1, 0, []
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            result.append(str(total%2))
+            carry = total//2
+            
+        return ''.join(result[::-1])
