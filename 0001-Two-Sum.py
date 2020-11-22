9f5dcf1b71fa5001683e97aa54dd3c7665674220
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-22 16:06:26 +0530

    Two Sum Hash Tables

--- 0001-Two-Sum.py
+++ 0001-Two-Sum.py
@@ -9,6 +9,9 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 '''
+
+# Using Hash Tables
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict = {}
