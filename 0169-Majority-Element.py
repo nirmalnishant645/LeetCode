8ea817d278bea503e91551724060f8193d8fda42
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-24 11:01:51 +0530

    Majority Element using Hash Table

--- 0169-Majority-Element.py
+++ 0169-Majority-Element.py
@@ -41,6 +41,18 @@ class Solution:
                 count += 1
         return res
 
+# Using Hash Table
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        for num in nums:
+            if num in d:
+                d[num] += 1
+            else:
+                d[num] = 1
+        return max(d, key = d.get)
+
 # Custom Input Check
 
 s = Solution()
