3cd9d2d3e72d7babed8de4f0059c74835773f518
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-11-30 11:19:42 +0530

    Odd Even Linked List ListNode Definition

--- 0328-Odd-Even-Linked-List.py
+++ 0328-Odd-Even-Linked-List.py
@@ -20,10 +20,12 @@ Note:
 
 '''
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
         if not head:
