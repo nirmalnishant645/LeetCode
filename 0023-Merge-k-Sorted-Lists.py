a2e7cd30797a4bbb09e902f548ac15a022dcc004
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-01 12:03:22 +0530

    Merge k Sorted Lists

--- 0023-Merge-k-Sorted-Lists.py
+++ 0023-Merge-k-Sorted-Lists.py
@@ -11,39 +11,39 @@ Input:
 ]
 Output: 1->1->2->3->4->4->5->6
 '''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+# Method 1
+
 class Solution:
+    
+    def mergeTwoLists(self, l1, l2):
+        res = cur = ListNode()
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return res.next
+        
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         if not lists:
             return None
-        if len(lists) == 1:
-            return lists[0]
-        
-        k = len(lists) - 1
-        l1 = lists[0]
-        while k:
-            l2 = lists[k]
-            l1 = self.mergeTwoLists(l1, l2)
-            k -= 1
-        return l1
-            
-            
-            
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1 or not l2: return l1 or l2
-        head = cur_node = ListNode(0)
-        while l1 and l2:
-            if l1.val < l2.val:
-                cur_node.next = l1
-                l1 = l1.next
-            else:
-                cur_node.next = l2
-                l2 = l2.next
-            cur_node = cur_node.next
-        cur_node.next = l1 or l2
-        return head.next
+        last = len(lists) - 1
+        while last:
+            i, j = 0, last
+            while i < j:
+                lists[i] = self.mergeTwoLists(lists[i], lists[j])
+                i += 1
+                j -= 1
+                last -= 1
+        return lists[0]
