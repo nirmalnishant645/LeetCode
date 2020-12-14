fe2cd862dc1b6d142fc57e5cae6f02fb48da0392
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-14 13:02:03 +0530

    Palindrome Paritioning using DFS

--- 0131-Palindrome-Partitioning.py
+++ 0131-Palindrome-Partitioning.py
@@ -14,3 +14,34 @@ Constraints:
 1 <= s.length <= 16
 s contains only lowercase English letters.
 '''
+
+# Using DFS
+
+class Solution:
+    
+    def isPalindrome(self, seg):
+        start, end = 0, len(seg) - 1
+        
+        while start < end:
+            if seg[start] != seg[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
+    
+    def dfs(self, s, temp, res):
+        if not s and temp:
+            res.append(temp[:])
+            return
+        for i in range(1, len(s) + 1):
+            seg = s[:i]
+            if self.isPalindrome(seg):
+                temp.append(seg)
+                self.dfs(s[i:], temp, res)
+                temp.pop()
+    
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        temp = []
+        self.dfs(s, temp, res)
+        return res
