78758f816b720d30a62bc3178f1b8c5545896ea3
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-17 20:14:22 +0530

    Single Number Solution

--- 0136-Single-Number.py
+++ 0136-Single-Number.py
@@ -24,7 +24,7 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
+        res = 0 
         for num in nums:
             res ^= num
         return res
