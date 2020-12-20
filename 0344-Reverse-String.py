dbe478111dbecdf340b486fa6b7c81016d41d67e
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-20 20:14:20 +0530

    Reverse String Solution

--- 0344-Reverse-String.py
+++ 0344-Reverse-String.py
@@ -16,11 +16,11 @@ Example 2:
 Input: ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
 '''
+
+# Without returning anything and modifying in place
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
         start, end = 0, len(s) - 1
         while start < end:
             s[start], s[end] = s[end], s[start]
