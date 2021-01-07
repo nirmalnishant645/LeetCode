78bd9f25c0ff3f79c894a9e6ebef6ac0cc0c062b
nirmalnishant645 <nirmalnishant645@gmail.com>
2021-01-07 14:15:37 +0530

    Peak Index Binary Search Solution

--- 0852-Peak-Index-In-A-Mountain-Array.py
+++ 0852-Peak-Index-In-A-Mountain-Array.py
@@ -23,3 +23,16 @@ Note:
 0 <= A[i] <= 10^6
 A is a mountain, as defined above.
 '''
+
+# Method 1
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        start, end = 0, len(arr) - 1
+        while start < end:
+            mid = (start + end) // 2
+            if arr[mid] > arr[mid + 1]:
+                end = mid
+            else:
+                start = mid + 1
+        return start
