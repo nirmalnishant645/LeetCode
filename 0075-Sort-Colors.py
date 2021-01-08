a9d6bb969621150a3a223bdb1ff72c0a283a9755
nirmalnishant645 <nirmalnishant645@gmail.com>
2021-01-08 21:41:52 +0530

    Sort Colors

--- 0075-Sort-Colors.py
+++ 0075-Sort-Colors.py
@@ -15,3 +15,17 @@ A rather straight forward solution is a two-pass algorithm using counting sort.
 First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
 Could you come up with a one-pass algorithm using only constant space?
 '''
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        index, start, end = 0, 0, len(nums) - 1
+        while index <= end and start < end:
+            if nums[index] == 0:
+                nums[index], nums[start] = nums[start], nums[index]
+                start += 1
+                index += 1
+            elif nums[index] == 2:
+                nums[index], nums[end] = nums[end], nums[index]
+                end -= 1
+            else:
+                index += 1
