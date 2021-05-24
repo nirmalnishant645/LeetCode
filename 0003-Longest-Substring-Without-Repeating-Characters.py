207f2ab077cad048c3539e11645c74e354fa2c9e
nirmalnishant645 <nirmalnishant645@gmail.com>
2021-05-24 13:23:52 +0530

    Longest Unique Substring(Two Pointers & HashTable)

--- 0003-Longest-Substring-Without-Repeating-Characters.py
+++ 0003-Longest-Substring-Without-Repeating-Characters.py
@@ -41,3 +41,18 @@ class Solution:
                 maxx = max(len(char), maxx)
         
         return maxx
+
+# Two Pointers and Hash Table
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = right = max_len = 0
+        d = {}
+        while left < len(s) and right < len(s):
+            if s[right] in d:
+                left = max(left, d[s[right]] + 1)
+            d[s[right]] = right
+            max_len = max(max_len, right - left + 1)
+            right += 1
+        return max_len
+
