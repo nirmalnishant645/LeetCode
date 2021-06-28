fa6d60a277a53ad8053c32489073525c67d4321a
Nishant Roche <nirmalnishant645@gmail.com>
2021-06-28 15:38:27 +0530

    Two Sum New

--- 0001-Two-Sum.py
+++ 0001-Two-Sum.py
@@ -25,6 +25,15 @@ class Solution:
 
 # Using Hash Tables (New)
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for index, num in enumerate(nums):
+            if target - num in d.keys():
+                return [d[target - num], index]
+            else:
+                d[num] = index
+
 # Check Custom Input
 
 s = Solution()
