6f45fb1538ca7472041647243c76decc344e011f
Nishant Roche <nirmalnishant645@gmail.com>
2021-07-01 15:40:26 +0530

    238 Postfix Prefix Product

--- 0238-Product-of-Array-Except-Self.py
+++ 0238-Product-of-Array-Except-Self.py
@@ -11,6 +11,11 @@ Note: Please solve it without division and in O(n).
 Follow up:
 Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
 '''
+
+# Using Postfix and Prefix Product
+
+# Similar method with small changes
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * (len(nums))
