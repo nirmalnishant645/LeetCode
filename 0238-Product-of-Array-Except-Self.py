671b246ace54b8fbbdec1d9a1dc965133c6de64b
Nishant Roche <nirmalnishant645@gmail.com>
2021-07-02 15:42:43 +0530

    238 similar solution

--- 0238-Product-of-Array-Except-Self.py
+++ 0238-Product-of-Array-Except-Self.py
@@ -14,6 +14,19 @@ Could you solve it with constant space complexity? (The output array does not co
 
 # Using Postfix and Prefix Product
 
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1]*len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
 # Similar method with small changes
 
 class Solution:
