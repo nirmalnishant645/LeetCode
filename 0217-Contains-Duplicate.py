8b9132b387da1c1c5e5039e527e89aa831aff4b3
Nishant Roche <nirmalnishant645@gmail.com>
2021-07-03 15:43:10 +0530

    Contains Duplicate with Hash Map

--- 0217-Contains-Duplicate.py
+++ 0217-Contains-Duplicate.py
@@ -31,15 +31,6 @@ class Solution:
 
 # Using Hash Map
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set()
-        for num in nums:
-            if num in s:
-                return True
-            s.add(num)
-        return False
-
 # Check Custom Input
 
 s = Solution()
