8bdb4583187ee181ca626063d7684dcc64c80be3
Nishant Roche <nirmalnishant645@gmail.com>
2022-07-13 11:57:18 +0530

    Search Insert Position

--- 0035-Search-Insert-Position.py
+++ 0035-Search-Insert-Position.py
@@ -21,17 +21,18 @@ Example 4:
 Input: [1,3,5,6], 0
 Output: 0
 '''
+
+# Using Binary Search
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        lower, upper = 0, len(nums)-1
-
-        while (lower<=upper):
-            mid = (lower+upper)//2
-
-            if (nums[mid]==target):
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
                 return mid
-            if (nums[mid]<target):
-                lower=mid+1
+            if target > nums[mid]:
+                left = mid + 1
             else:
-                upper=mid-1
-        return lower
+                right = mid
+        return left
